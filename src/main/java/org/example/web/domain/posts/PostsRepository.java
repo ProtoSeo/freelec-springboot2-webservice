3203e88d834134e2c74d10d8721be01527d8c38d
SeoSeunghun <proto_seo@naver.com>
2021-02-23 14:21:40 +0900

    04. 머스테치로 화면 구성하기.md

--- src/main/java/org/example/web/domain/posts/PostsRepository.java
+++ src/main/java/org/example/web/domain/posts/PostsRepository.java
@@ -1,7 +1,11 @@
 package org.example.web.domain.posts;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface PostsRepository extends JpaRepository<Posts,Long> {
-
+    @Query("SELECT p from Posts p ORDER BY p.id desc")
+    List<Posts> findAllDesc();
 }
